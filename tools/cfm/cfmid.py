473ed7a0793ab8fd388a5267c6623e54f91dca91
RJMW <r.j.weber@bham.ac.uk>
2019-03-07 16:51:09 +0000

    Update wrapper, rename files and fix python script

--- tools/cfm/cfmid.py
+++ tools/cfm/cfmid.py
@@ -1,6 +1,8 @@
 import argparse
 import csv
 import os
+import shutil
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input')
@@ -15,25 +17,31 @@ parser.add_argument('--results')
 parser.add_argument('--tool_directory')
 
 args = parser.parse_args()
-print args
+print(args)
+
+# Example
+# python cfmid.py --abs_mass_tol='0.01' --db_local='test-data/demo_db.tsv' --input='test-data/input.msp' --num_highest='10' --polarity='pos' --ppm_db='10.0' --ppm_mass_tol='10.0' --results='results.txt' --score_type='Jaccard' --tool_directory='/home/rw/Documents/cfm-galaxy/tools/cfm'
 
 id2info = {}
 mz2id = []
 #store DB in dicts
 with open(args.db_local) as csvfile:
-    csvreader = csv.DictReader(csvfile)
+    csvreader = csv.DictReader(csvfile, delimiter='\t')
     for row in csvreader:
         id2info[row["Identifier"]] = row
-        mz2id.append(
-           (float(row["MonoisotopicMass"]),
-           row["Identifier"]))
+        mz2id.append((float(row["MonoisotopicMass"]), row["Identifier"]))
 
-os.makedirs("tempf")
-with open(args.input,"r") as infile:
+name_tmp = "tmp"
+if os.path.isdir(name_tmp):
+    shutil.rmtree(name_tmp)
+
+os.makedirs(name_tmp)
+with open(args.input, "r") as infile:
     numlines = 0
     for line in infile:
         line = line.strip()
         if numlines == 0:
+            print(line)
             if "NAME" in line:
                 featid = line.split("NAME: ")[1]
             if "PRECURSORMZ" in line:
@@ -68,20 +76,21 @@ with open(args.input,"r") as infile:
                     with open('./tmpcand.txt', 'w') as outfile:
                         for c in cand_id_list:
                             outfile.write("{0} {1}\n".format(c,id2info[c]["InChI"])) #TODO: Use InChI or SMILES
-                    #create commandline input
-                    outi = "tempf/cfm_" + featid + ".txt" 
-                    cmd_command = args.tool_directory+"/cfmid/bin/cfm-id tmpspec.txt {0} tmpcand.txt ".format(featid)
+
+                    #create command line call
+                    outi = os.path.join(name_tmp, "cfm_" + featid + ".txt")
+                    cmd_command = "cfm-id tmpspec.txt {0} tmpcand.txt ".format(featid)
                     cmd_command += "{0} {1} {2} {3} ".format(
                         args.num_highest, args.ppm_db, args.ppm_mass_tol, args.abs_mass_tol) 
                     if args.polarity == "pos":
-                        cmd_command += args.tool_directory+"/cfmid/positive_metab_se_cfm/param_output0.log "
-                        cmd_command += args.tool_directory+"/cfmid/positive_metab_se_cfm/param_config.txt "
+                        cmd_command += os.path.join(args.tool_directory, "data", "positive_metab_se_cfm", "param_output0.log") + " "
+                        cmd_command += os.path.join(args.tool_directory, "data", "positive_metab_se_cfm", "param_config.txt") + " "
                     else:
-                        cmd_command += args.tool_directory+"/cfmid/negative_metab_se_cfm/param_output0.log "
-                        cmd_command += args.tool_directory+"/cfmid/negative_metab_se_cfm/param_config.txt "
+                        cmd_command += os.path.join(args.tool_directory, "data", "negative_metab_se_cfm/param_output0.log") + " "
+                        cmd_command += os.path.join(args.tool_directory, "data", "negative_metab_se_cfm/param_config.txt") + " "
                     cmd_command += "{0} 1 {1}".format(args.score_type, outi)
                     # run
-                    print cmd_command
+                    print(cmd_command)
                     os.system(cmd_command)
             else:
                 line = tuple(line.split("\t"))
@@ -90,14 +99,15 @@ with open(args.input,"r") as infile:
 
 
 #merge outputs
-outfiles = os.listdir("tempf")
+outfiles = os.listdir(name_tmp)
+outfiles.sort(key=lambda x: os.path.getmtime(os.path.join(name_tmp, x)))
 with open(args.results, 'w') as outfile:
     outfile.write("UID\tRank\tScore\tIdentifier\tInChI\n")
     for fname in outfiles:
         fileid = os.path.basename(fname)
         fileid = fileid.split("_")[1]
         fileid = fileid.split(".txt")[0]
-        with open("./tempf/"+fname) as infile:
+        with open(os.path.join(name_tmp, fname)) as infile:
             for line in infile:
                 line=line.replace(" ","\t")
                 outfile.write(fileid+"\t"+line)
